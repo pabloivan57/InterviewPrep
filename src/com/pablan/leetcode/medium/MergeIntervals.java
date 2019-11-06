8062fbbb77e28db41e669e3f99444da06432e5c6
Pablo Ochoa <pablan@amazon.com>
2019-11-06 09:11:34 -0800

    Three Sum more details and added description to MergeIntervals

--- src/com/pablan/leetcode/medium/MergeIntervals.java
+++ src/com/pablan/leetcode/medium/MergeIntervals.java
@@ -16,6 +16,15 @@ public class MergeIntervals {
         }
     }
 
+    /**
+     * Given a collection of intervals, merge all overlapping intervals.
+     *
+     * Example 1:
+     *
+     * Input: [[1,3],[2,6],[8,10],[15,18]]
+     * Output: [[1,6],[8,10],[15,18]]
+     * Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+     */
     public int[][] merge(int[][] intervals) {
         List<Interval> intervalsList = new ArrayList<>();
 
--- src/com/pablan/leetcode/medium/ThreeSum.java
+++ src/com/pablan/leetcode/medium/ThreeSum.java
@@ -2,46 +2,71 @@ package com.pablan.leetcode.medium;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ThreeSum {
 
-    //Given array nums = [-1, 0, 1, 2, -1, -4],
-    //
-    //A solution set is:
-    //[
-    //  [-1, 0, 1],
-    //  [-1, -1, 2]
-    //]
+    /**
+     * Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
+     * Find all unique triplets in the array which gives the sum of zero.
+     *
+     * Note:
+     *
+     * The solution set must not contain duplicate triplets.
+     *
+     * Example:
+     *
+     * Given array nums = [-1, 0, 1, 2, -1, -4],
+     *
+     * A solution set is:
+     * [
+     *   [-1, 0, 1],
+     *   [-1, -1, 2]
+     * ]
+     */
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
+        if(nums == null || nums.length == 0) {
+            return Collections.emptyList();
+        }
 
         List<List<Integer>> result = new ArrayList<>();
 
+        Arrays.sort(nums);
+
         for(int i = 0; i < nums.length; i++) {
-            if(i > 0 && nums[i - 1] == nums[i]) {
-                continue;
+            if(i >= 1) {
+                if(nums[i - 1] == nums[i]) {
+                    // In this case we are going to try to find same target (chances we will get same triplet)
+                    // so we skip
+                    continue;
+                }
             }
+
             findTriplet(i + 1, -nums[i], nums, result);
         }
 
         return result;
     }
 
-    private void findTriplet(int start, int target, int[] nums, List<List<Integer>> triplets) {
+    private void findTriplet(int start, int target, int[] nums, List<List<Integer>> result) {
         int p1 = start;
         int p2 = nums.length - 1;
 
         while(p1 < p2) {
-            if(nums[p1] + nums[p2] == target) {
-                List<Integer> result = new ArrayList<>();
-                result.add(nums[p1]);
-                result.add(nums[p2]);
-                result.add(-target);
+            int sum = nums[p1] + nums[p2];
 
-                triplets.add(result);
+            if(sum == target) {
+                // found a triplet
+                List<Integer> triplet = new ArrayList<>();
+                triplet.add(-target);
+                triplet.add(nums[p1]);
+                triplet.add(nums[p2]);
+
+                result.add(triplet);
+                // attempt to find another triplet in same set
                 p1++;
-            } else if(nums[p1] + nums[p2] > target) {
+            } else if (sum > target) {
                 p2--;
             } else {
                 p1++;
