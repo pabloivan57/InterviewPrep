0eadeb20104f43a0a98a561ff82ebee8c86602fb
Pablo Ochoa <pablan@amazon.com>
2019-11-06 08:33:14 -0800

    Added comment to MaximumSubarray

--- src/com/pablan/leetcode/easy/MaximumSubarray.java
+++ src/com/pablan/leetcode/easy/MaximumSubarray.java
@@ -17,16 +17,9 @@ public class MaximumSubarray {
 
     current element = [-2,1,-3,4,-1,2,1,-5,4]
     current max sum = [-2,1,-2,4,3,5,6,1,5]
-    max seen so far = [-2,1,1,4,4,5,6,6,6] --> last one is the answer
+    max seen so far from max sum = [-2,1,1,4,4,5,6,6,6] --> last one is the answer
      */
     public int maxSubArray(int[] nums) {
-        int n = nums.length;
-        int currSum = nums[0], maxSum = nums[0];
 
-        for(int i = 1; i < n; ++i) {
-            currSum = Math.max(nums[i], currSum + nums[i]);
-            maxSum = Math.max(maxSum, currSum);
-        }
-        return maxSum;
     }
 }
