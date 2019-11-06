407c21f991dbde82afc0487a2c0605b9e8d0860a
Pablo Ochoa <pablan@amazon.com>
2019-11-06 08:39:33 -0800

    MaximumSubarrayProblem

--- src/com/pablan/leetcode/easy/MaximumSubarray.java
+++ src/com/pablan/leetcode/easy/MaximumSubarray.java
@@ -13,13 +13,25 @@ public class MaximumSubarray {
 
     Note: This problem looks like Sliding window, but it's not...
 
-    Look at this explanantion:
+    Look at this explanation:
 
     current element = [-2,1,-3,4,-1,2,1,-5,4]
     current max sum = [-2,1,-2,4,3,5,6,1,5]
     max seen so far from max sum = [-2,1,1,4,4,5,6,6,6] --> last one is the answer
      */
     public int maxSubArray(int[] nums) {
+        int[] maxSums = new int[nums.length];
+        // Initialize first element
+        maxSums[0] = nums[0];
+        int maxSum = nums[0];
 
+        for(int i = 1; i < nums.length; i++) {
+            maxSums[i] = Math.max(maxSums[i - 1] + nums[i], nums[i]);
+            if(maxSums[i] > maxSum) {
+                maxSum = maxSums[i];
+            }
+        }
+
+        return maxSum;
     }
 }
---
+++ tst/com/pablan/leetcode/easy/MaximumSubarrayTest.java
@@ -0,0 +1,12 @@
+package com.pablan.leetcode.easy;
+
+import org.junit.Test;
+
+public class MaximumSubarrayTest {
+
+    @Test
+    public void test() {
+        MaximumSubarray maximumSubarray = new MaximumSubarray();
+        System.out.println(maximumSubarray.maxSubArray(new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+}
