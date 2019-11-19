ec0c50d0a950a81a85e38439f1b23f10de0546c9
Pablo Ochoa <pablan@amazon.com>
2019-11-19 12:44:35 -0800

    Add LinkedListCycle problem

--- src/com/pablan/leetcode/easy/LinkedListCycle.java
+++ src/com/pablan/leetcode/easy/LinkedListCycle.java
@@ -4,6 +4,20 @@ import com.pablan.grokking.patterns.fastslow.ListNode;
 
 public class LinkedListCycle {
 
+    /**
+     * Given a linked list, determine if it has a cycle in it.
+     *
+     * To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: head = [3,2,0,-4], pos = 1
+     * Output: true
+     * Explanation: There is a cycle in the linked list, where tail connects to the second node.
+     *
+     */
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
