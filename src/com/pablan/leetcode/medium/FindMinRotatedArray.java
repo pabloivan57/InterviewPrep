69f50ab04ca6e21cc58c1d4a8624bd89f8b5d451
Pablo Ochoa <pablan@amazon.com>
2019-11-19 17:16:41 -0800

    Add FindMinRotatedArray problem details

--- src/com/pablan/leetcode/medium/FindMinRotatedArray.java
+++ src/com/pablan/leetcode/medium/FindMinRotatedArray.java
@@ -1,6 +1,22 @@
 package com.pablan.leetcode.medium;
 
 public class FindMinRotatedArray {
+
+    /**
+     * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+     *
+     * (i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+     *
+     * Find the minimum element.
+     *
+     * You may assume no duplicate exists in the array.
+     *
+     * Example 1:
+     *
+     * Input: [3,4,5,1,2]
+     * Output: 1
+     *
+     */
     public int findMin(int[] nums) {
        if(nums.length == 1) {
            return nums[0];
