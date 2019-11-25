4966a7d7101c926f94e0dcf3c5d34c5b014982d7
Pablo Ochoa <pablan@amazon.com>
2019-11-25 12:47:21 -0800

    Palindromic Substrings comments

--- src/com/pablan/leetcode/medium/PalindromicSubstrings.java
+++ src/com/pablan/leetcode/medium/PalindromicSubstrings.java
@@ -17,6 +17,10 @@ public class PalindromicSubstrings {
      * Output: 6
      * Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
      *
+     * Pablo's notes: - For naive approach (at least something, start expanding at each center and you will find
+     * all palindromes, but in O(n^2))
+     * For linear time, you will have to use Manacher's algorithm
+     *
      */
     public int countSubstrings(String s) {
         return 0;
