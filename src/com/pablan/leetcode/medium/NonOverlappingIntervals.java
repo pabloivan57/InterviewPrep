62c2942e8877c549a46f3ad863c1af4e5cd237c2
Pablo Ochoa <pablan@amazon.com>
2019-11-26 08:46:01 -0800

    NonOverlappingIntervals problem details

--- src/com/pablan/leetcode/medium/NonOverlappingIntervals.java
+++ src/com/pablan/leetcode/medium/NonOverlappingIntervals.java
@@ -8,38 +8,28 @@ import java.util.List;
 
 public class NonOverlappingIntervals {
 
+    /**
+     * Given a collection of intervals, find the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: [[1,2],[2,3],[3,4],[1,3]]
+     * Output: 1
+     * Explanation: [1,3] can be removed and the rest of intervals are non-overlapping.
+     * Example 2:
+     *
+     * Input: [[1,2],[1,2],[1,2]]
+     * Output: 2
+     * Explanation: You need to remove two [1,2] to make the rest of intervals non-overlapping.
+     *
+     */
     public int eraseOverlapIntervals(int[][] intervals) {
-        List<Interval> intervalList = new ArrayList<>();
 
-        for(int i = 0; i < intervals.length; i++) {
-            intervalList.add(new Interval(intervals[i][0], intervals[i][1]));
-        }
-
-        return eraseOverlapIntervals(intervalList);
     }
 
     private int eraseOverlapIntervals(List<Interval> intervals) {
-        if(intervals == null || intervals.isEmpty()) return 0;
 
-        Collections.sort(intervals, (a, b) -> a.start - b.start);
-
-        int removals = 0;
-        Interval interval = intervals.get(0);
-        for(int i = 1; i < intervals.size(); i++) {
-            Interval currentInterval = intervals.get(i);
-
-            if(interval.end <= currentInterval.start) {
-                // they don't overlap, continue
-                interval = currentInterval;
-                continue;
-            } else {
-                if(currentInterval.end < interval.end) {
-                    interval = currentInterval;
-                }
-                removals++;
-            }
-        }
-
-        return removals;
     }
 }
