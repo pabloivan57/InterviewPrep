3888082c263a336f2434002661b6218c7dd50986
Pablo Ochoa <pablan@amazon.com>
2019-12-02 18:00:44 -0800

    Resolve Construct Binary Preorder Inorder tree

--- src/com/pablan/leetcode/medium/ConstructBinaryPreorderInorder.java
+++ src/com/pablan/leetcode/medium/ConstructBinaryPreorderInorder.java
@@ -2,7 +2,20 @@ package com.pablan.leetcode.medium;
 
 import com.pablan.grokking.patterns.treebfs.TreeNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConstructBinaryPreorderInorder {
+
+    Map<Integer, Integer> inOrderIndexes;
+    int preOrderIndex = 0;
+    int[] preOrder = null;
+    int[] inOrder = null;
+
+    public ConstructBinaryPreorderInorder() {
+        this.inOrderIndexes = new HashMap<>();
+        preOrderIndex = 0;
+    }
 
     /**
      * Given preorder and inorder traversal of a tree, construct the binary tree.
@@ -22,8 +35,6 @@ public class ConstructBinaryPreorderInorder {
      *     /  \
      *    15   7
      *
-     * preorder = [3,9,20,15,7]
-     * inorder = [9,3,15,20,7]
      *
      * Pablo's notes: The two of them together have an interesting property
      * the value that you pick in preorder divides the inOrder in half,
@@ -33,6 +44,30 @@ public class ConstructBinaryPreorderInorder {
      *       9        15,20,7 -> to be arranged
      */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preOrder = preorder;
+        this.inOrder = inorder;
 
+        for(int i = 0; i < preorder.length; i++) {
+            inOrderIndexes.put(inorder[i], i);
+        }
+
+        return createTree(0, preOrder.length);
+    }
+
+    private TreeNode createTree(int leftPointer, int rightPointer) {
+        if(leftPointer == rightPointer) {
+            return null;
+        }
+
+        int rootValue = preOrder[preOrderIndex];
+        preOrderIndex++;
+        TreeNode node = new TreeNode(rootValue);
+
+        int splitIndex = inOrderIndexes.get(rootValue); // a.k.a current inOrder index
+
+        node.left = createTree(leftPointer, splitIndex);
+        node.right = createTree(splitIndex + 1, rightPointer);
+
+        return node;
     }
 }
--- tst/com/pablan/leetcode/medium/ConstructBinaryPreorderInorderTest.java
+++ tst/com/pablan/leetcode/medium/ConstructBinaryPreorderInorderTest.java
@@ -1,11 +1,17 @@
 package com.pablan.leetcode.medium;
 
+import com.pablan.grokking.patterns.treebfs.TreeNode;
 import org.junit.Test;
 
 public class ConstructBinaryPreorderInorderTest {
 
     @Test
     public void test() {
+        ConstructBinaryPreorderInorder constructBinaryPreorderInorder = new ConstructBinaryPreorderInorder();
 
+        int[] preorder = new int[] {3,9,20,15,7};
+        int[] inorder = new int[] {9,3,15,20,7};
+        TreeNode root = constructBinaryPreorderInorder.buildTree(preorder, inorder);
+        System.out.println(root);
     }
 }
