6d1aadef6ec61d301c8d5ec0e62cde290b9580ea
Pablo Ochoa <pablan@amazon.com>
2019-12-04 20:03:45 -0800

    Add CLone Graph Details

--- src/com/pablan/leetcode/medium/CloneGraph.java
+++ src/com/pablan/leetcode/medium/CloneGraph.java
@@ -10,6 +10,10 @@ import java.util.Queue;
 
 public class CloneGraph {
 
+    /**
+     * Given a reference of a node in a connected undirected graph, return a deep copy (clone) of the graph.
+     * Each node in the graph contains a val (int) and a list (List[Node]) of its neighbors.
+     */
     public Node cloneGraph(Node node) {
         if(node == null) return null;
 
