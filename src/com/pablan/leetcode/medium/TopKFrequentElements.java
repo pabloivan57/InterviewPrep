605c6b6307876a1888eddd7c58703bf3c7627124
Pablo Ochoa <pablan@amazon.com>
2019-12-04 17:42:48 -0800

    Add Top K Frequent exercise details

--- src/com/pablan/leetcode/medium/TopKFrequentElements.java
+++ src/com/pablan/leetcode/medium/TopKFrequentElements.java
@@ -9,6 +9,19 @@ import java.util.PriorityQueue;
 
 public class TopKFrequentElements {
 
+    /**
+     * Given a non-empty array of integers, return the k most frequent elements.
+     *
+     * Example 1:
+     *
+     * Input: nums = [1,1,1,2,2,3], k = 2
+     * Output: [1,2]
+     * Example 2:
+     *
+     * Input: nums = [1], k = 1
+     * Output: [1]
+     *
+     */
     public List<Integer> topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> occurrences = new HashMap<>();
 
