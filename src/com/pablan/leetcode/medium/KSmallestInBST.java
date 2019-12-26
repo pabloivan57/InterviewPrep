09f0bd1cb70ebf6646010d9e4a8431634ace58a5
Pablo Ochoa <pablan@amazon.com>
2019-12-26 12:27:47 -0800

    KSmallest in BST details

--- src/com/pablan/leetcode/medium/KSmallestInBST.java
+++ src/com/pablan/leetcode/medium/KSmallestInBST.java
@@ -8,6 +8,34 @@ import java.util.List;
 
 public class KSmallestInBST {
 
+    /**
+     * Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+     *
+     * Note:
+     * You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+     *
+     * Example 1:
+     *
+     * Input: root = [3,1,4,null,2], k = 1
+     *    3
+     *   / \
+     *  1   4
+     *   \
+     *    2
+     * Output: 1
+     * Example 2:
+     *
+     * Input: root = [5,3,6,2,4,null,null,1], k = 3
+     *        5
+     *       / \
+     *      3   6
+     *     / \
+     *    2   4
+     *   /
+     *  1
+     * Output: 3
+     *
+     */
     public int kthSmallest(TreeNode root, int k) {
         List<Integer> list = new ArrayList<>();
         inOrder(root, list);
