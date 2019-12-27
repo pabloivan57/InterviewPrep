582817dd9829c5e27ae714080b1b39614b4ceb27
Pablo Ochoa <pablan@amazon.com>
2019-12-26 16:29:40 -0800

    Merge K Sorted Lists problem details

--- src/com/pablan/leetcode/facebook/MergeKSortedLists.java
+++ src/com/pablan/leetcode/facebook/MergeKSortedLists.java
@@ -5,6 +5,20 @@ import java.util.Queue;
 
 public class MergeKSortedLists {
 
+    /**
+     * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+     *
+     * Example:
+     *
+     * Input:
+     * [
+     *   1->4->5,
+     *   1->3->4,
+     *   2->6
+     * ]
+     * Output: 1->1->2->3->4->4->5->6
+     *
+     */
     public ListNode mergeKLists(ListNode[] lists) {
         Queue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
 
