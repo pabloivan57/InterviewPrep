3971a7060c77be37a08cf2615c0eb42473467ff6
Pablo Ochoa <pablan@amazon.com>
2019-12-27 17:19:53 -0800

    Longest Consecutive Sequence Explanation

--- src/com/pablan/leetcode/hard/LongestConsecutiveSequence.java
+++ src/com/pablan/leetcode/hard/LongestConsecutiveSequence.java
@@ -16,12 +16,17 @@ public class LongestConsecutiveSequence {
      * Output: 4
      * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
      *
-     * Note: This example would help understad
+     * Pablo's notes: The idea is to find the beginning of those sequences. You check if the number before current
+     * is present to discard finding a number part of a sequence. For example: 100, is there a 99? No, ok so this is a
+     * sequence... we proceed. Is there a 101? Nope, ok so this was a sequence 1
      *
-     * "wouldn't [100,99, 98, ..., 1] (i.e. an array in reverse order)
-     * make this run in $$O(n^2)$$ because for every num, it have to go through the entire set so far?"
+     * In the case of 4, is there a 3? Yes, is there a 2? Yes, is there a 1? Yes, is there a 0? nope... the proceed
+     * 1, 2, 3, 4 --> there is no 5, ok so max length is 4
      *
-     * ""
+     * In the case of 200 is the same as 100, Then in the case of 1 we already have that one! (Hint: this is optimized
+     * by dynamic programming solution) but this algorithm calculates it again, we do the same with 3 and 2. Max length
+     * is 4
+     *
      */
     public int longestConsecutive(int[] nums) {
         Set<Integer> numSet = new HashSet<>();
