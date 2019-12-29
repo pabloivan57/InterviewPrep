018b5d72e7692ac5891801803faac2d40c6d8a93
Pablo Ochoa <pablan@amazon.com>
2019-12-29 00:13:00 -0800

    Word Break Problem

---
+++ tst/com/pablan/leetcode/easy/WordBreakTest.java
@@ -0,0 +1,21 @@
+package com.pablan.leetcode.easy;
+
+import com.pablan.leetcode.medium.WordBreak;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordBreakTest {
+
+    @Test
+    public void test() {
+        WordBreak wordBreak = new WordBreak();
+        List<String> dict = new ArrayList<>();
+        dict.add("cats");
+        dict.add("and");
+        dict.add("dog");
+
+        System.out.println(wordBreak.wordBreak("catsanddog", dict));
+    }
+}
