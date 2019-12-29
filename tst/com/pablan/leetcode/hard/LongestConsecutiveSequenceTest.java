cf2bd5debdbf479c732662cea5f0601e50d28d59
Pablo Ochoa <pablan@amazon.com>
2019-12-28 22:44:04 -0800

    Longest Consecutive Sequence Test

---
+++ tst/com/pablan/leetcode/hard/LongestConsecutiveSequenceTest.java
@@ -0,0 +1,12 @@
+package com.pablan.leetcode.hard;
+
+import org.junit.Test;
+
+public class LongestConsecutiveSequenceTest {
+
+    @Test
+    public void test() {
+        LongestConsecutiveSequence longestConsecutiveSequence = new LongestConsecutiveSequence();
+        System.out.println(longestConsecutiveSequence.longestConsecutive(new int[] {100, 4, 200, 1, 3, 2}));
+    }
+}
