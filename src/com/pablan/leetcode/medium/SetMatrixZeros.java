7aa77e323b3ef8b804eee515855eb2b9f9a72e8f
Pablo Ochoa <pablan@amazon.com>
2020-01-10 08:38:17 -0800

    Set Matrix Zeroes example

--- src/com/pablan/leetcode/medium/SetMatrixZeros.java
+++ src/com/pablan/leetcode/medium/SetMatrixZeros.java
@@ -38,8 +38,22 @@ public class SetMatrixZeros {
      * A straight forward solution using O(mn) space is probably a bad idea.
      * A simple improvement uses O(m + n) space, but still not the best solution.
      * Could you devise a constant space solution?
+     *
+     * Pablo's notes: 2 solutions, 2nd one is clever
+     *
+     * 1) Common sense, the problem when you change the whole column - row to 0's is that
+     * you still have to iterate each element, and that might confuse your algorithm changing the whole
+     * row/ column from one of the new zeroes are not supposed to. To overcome this problem, we choose another
+     * flag to mark cells that will have to be change to 0 later (-1000 for example). Later we iterate again the
+     * matrix and modify all the zeroes
+     *
+     * 2) An improvement on one, the problem before is that while setting -1000 the cell can be visited multiple times
+     * when it's not really needed to. What is clever about this approach is that it just 'takes note' of the rows
+     * and columns that have to be changed by marking [i][0] and [j][0]. Then we just iterate again over the matrix
+     * and check if [i][0] or [j][0] == '0'. If that's the case then we proceed to make the cell 0
      */
     public void setZeroes(int[][] matrix) {
+
 
     }
 }
