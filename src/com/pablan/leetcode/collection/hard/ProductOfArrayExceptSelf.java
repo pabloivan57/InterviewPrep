f0715b7ccda6ab41b6ead519123c6de4636bf347
Pablo Ochoa <pablan@amazon.com>
2020-01-13 17:25:50 -0800

    ProductOfArraExceptSelf

--- src/com/pablan/leetcode/collection/hard/ProductOfArrayExceptSelf.java
+++ src/com/pablan/leetcode/collection/hard/ProductOfArrayExceptSelf.java
@@ -17,6 +17,27 @@ public class ProductOfArrayExceptSelf {
      * (The output array does not count as extra space for the purpose of space complexity analysis.)
      */
     public int[] productExceptSelf(int[] nums) {
+        int[] leftProduct = new int[nums.length];
+        int[] rightProduct = new int[nums.length];
+        int[] result = new int[nums.length];
 
+        leftProduct[0] = nums[0];
+        rightProduct[nums.length - 1] = nums[nums.length - 1];
+
+        for(int i = 1; i < nums.length; i++) {
+            leftProduct[i] = nums[i] * leftProduct[i - 1];
+        }
+
+        for(int j = nums.length - 2; j >= 0; j--) {
+            rightProduct[j] = nums[j] * rightProduct[j + 1];
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            int left = i - 1 < 0 ? 1 : leftProduct[i - 1];
+            int right = i + 1 > nums.length - 1 ? 1 : rightProduct[i + 1];
+            result[i] = left * right;
+        }
+
+        return result;
     }
 }
--- tst/com/pablan/leetcode/collection/hard/ProductOfArrayExceptSelfTest.java
+++ tst/com/pablan/leetcode/collection/hard/ProductOfArrayExceptSelfTest.java
@@ -1,4 +1,14 @@
 package com.pablan.leetcode.collection.hard;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class ProductOfArrayExceptSelfTest {
+
+    @Test
+    public void test() {
+        ProductOfArrayExceptSelf productOfArrayExceptSelf = new ProductOfArrayExceptSelf();
+        System.out.println(Arrays.toString(productOfArrayExceptSelf.productExceptSelf(new int[] {1,2,3,4})));
+    }
 }
