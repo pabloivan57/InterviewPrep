44d7287e574e184d4348daf4e30a35e9ebb78ed5
Pablo Ochoa <pablan@amazon.com>
2020-01-14 16:10:11 -0800

    Wor d Ladder

---
+++ tst/com/pablan/leetcode/collection/hard/WordLadderTest.java
@@ -0,0 +1,21 @@
+package com.pablan.leetcode.collection.hard;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordLadderTest {
+
+    @Test
+    public void test() {
+        WordLadder wordLadder = new WordLadder();
+        List<String> dict = new ArrayList<>();
+        dict.add("hot");
+        dict.add("dot");
+        dict.add("dog");
+        dict.add("lot");
+        dict.add("cog");
+        wordLadder.ladderLength("hit", "cog", dict);
+    }
+}
