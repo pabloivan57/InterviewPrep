2627832784e690f1e864768262dd54ac1821e830
Pablo Ochoa <pablan@amazon.com>
2020-01-21 17:47:13 -0800

    Remove Invalid Parentheses details

--- src/com/pablan/leetcode/facebook/RemoveInvalidParentheses.java
+++ src/com/pablan/leetcode/facebook/RemoveInvalidParentheses.java
@@ -9,6 +9,25 @@ public class RemoveInvalidParentheses {
 
     private Set<String> validExpressions = new HashSet<>();
 
+    /**
+     * Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+     *
+     * Note: The input string may contain letters other than the parentheses ( and ).
+     *
+     * Example 1:
+     *
+     * Input: "()())()"
+     * Output: ["()()()", "(())()"]
+     * Example 2:
+     *
+     * Input: "(a)())()"
+     * Output: ["(a)()()", "(a())()"]
+     * Example 3:
+     *
+     * Input: ")("
+     * Output: [""]
+     *
+     */
     public List<String> remove(String s) {
         validExpressions =  new HashSet<>();
         int left = 0;
