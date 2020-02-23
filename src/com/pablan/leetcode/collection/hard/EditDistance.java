985f96b08e49fe54304e7069bf2452183e349cc4
Pablo Ochoa <pablan@amazon.com>
2020-02-23 12:55:40 -0800

    Correct Edit Distance DP

--- src/com/pablan/leetcode/collection/hard/EditDistance.java
+++ src/com/pablan/leetcode/collection/hard/EditDistance.java
@@ -28,56 +28,56 @@ public class EditDistance {
      *
      */
     public int findMinOperations(String s1, String s2) {
-        return findMinOperations(s1, s2, 0, 0, 0);
+        //return findMinOperations(0, s1, 0, s2, 0);
+        return findMinOperationsDp(s1, s2);
     }
 
-    private int findMinOperations(String s1, String s2, int idx1, int idx2, int operations) {
-        // If we reached the end of 1 of the strings
-        if(idx1 >= s1.length() || idx2 >= s2.length()) {
+    private int findMinOperations(int index1, String s1, int index2, String s2, int operations) {
+        // if one of the strings is out of boundaries prune
+        if(index1 >= s1.length() || index2 >= s2.length()) {
             return Integer.MAX_VALUE;
         }
 
-        // Base case if we are at the end and both match
-        boolean equal = s1.charAt(idx1) == s2.charAt(idx2);
-        if(idx1 == s1.length() - 1 && idx2 == s2.length() - 1 && equal) {
+        // base case, if we are at the end and both are equal
+        boolean equal = s1.charAt(index1) == s2.charAt(index2);
+        if(index1 == s1.length() - 1 && index2 == s2.length() - 1 && equal) {
             return operations;
         }
 
-        // If both are equal
+        // equal
         if(equal) {
-            return findMinOperations(s1, s2, idx1 + 1, idx2 + 1, operations);
+            return findMinOperations(index1 + 1, s1, index2 + 1, s2, operations);
         }
 
-        // else
-        // case1: delete s1
-        // case2: delete s2
-        // case3: replace
-        int case1 = findMinOperations(s1, s2, idx1 + 1, idx2, operations + 1);
-        int case2 = findMinOperations(s1, s2, idx1, idx2 + 1, operations + 1);
-        int case3 = findMinOperations(s1, s2, idx1 + 1, idx2 +1, operations + 1);
+        // case1 replace
+        int case1 = findMinOperations(index1 + 1, s1, index2 + 1, s2, operations + 1);
+        // case2 delete s1
+        int case2 = findMinOperations(index1 + 1, s1, index2, s2, operations + 1);
+        // case3 delete s2
+        int case3 = findMinOperations(index1, s1, index2 + 1, s2, operations + 1);
 
-        return Math.min(Math.min(case1, case2), case3);
+        return Math.min(case1, Math.min(case2, case3));
     }
 
-    public int findMinOperationsBottomUp(String s1, String s2) {
+    private int findMinOperationsDp(String s1, String s2) {
         int[][] dp = new int[s1.length() + 1][s2.length() + 1];
 
-        // Base case1 amount of operations to match s1 with ""
+        // base cases compare s1 to ""
         for(int i = 0; i <= s1.length(); i++) {
             dp[i][0] = i;
         }
 
-        // Base case2 amount of operations to match "" with s2
-        for(int i = 0; i <= s2.length(); i++) {
+        // base cases compare s2 to ""
+        for(int i = 0 ; i <= s2.length(); i++) {
             dp[0][i] = i;
         }
 
-        for(int i = 1; i < s1.length(); i++) {
-            for(int j = 1; j < s2.length(); j++) {
-                if(s1.charAt(i) == s2.charAt(j)) {
+        for(int i = 1; i <= s1.length(); i++) {
+            for(int j = 1; j <= s2.length(); j++) {
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
                 }
             }
         }
