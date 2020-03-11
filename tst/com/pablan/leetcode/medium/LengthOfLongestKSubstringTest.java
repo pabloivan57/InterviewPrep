51fe3f197e4efb5a971ad0440ef16dbda98df6d1
Pablo Ochoa <pablan@amazon.com>
2020-03-11 16:51:22 -0700

    LengthOfLongestKSubstringTest improvements

--- tst/com/pablan/leetcode/medium/LengthOfLongestKSubstringTest.java
+++ tst/com/pablan/leetcode/medium/LengthOfLongestKSubstringTest.java
@@ -7,6 +7,6 @@ public class LengthOfLongestKSubstringTest {
     @Test
     public void test() {
         LengthOfLongestKSubstring lengthOfLongestKSubstring = new LengthOfLongestKSubstring();
-        System.out.println(lengthOfLongestKSubstring.lengthOfLongestSubstringKDistinct("eceba", 2));
+        System.out.println(lengthOfLongestKSubstring.lengthOfLongestSubstringKDistinct("eceba", 2)); // should be 3
     }
 }
