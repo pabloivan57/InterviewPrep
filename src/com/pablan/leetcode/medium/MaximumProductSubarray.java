7c063a44756533599663c03c0445fe56be46215f
Pablo Ochoa <pablan@amazon.com>
2020-03-19 18:00:14 -0700

    Maximum Product Subarray

--- src/com/pablan/leetcode/medium/MaximumProductSubarray.java
+++ src/com/pablan/leetcode/medium/MaximumProductSubarray.java
@@ -31,23 +31,14 @@ public class MaximumProductSubarray {
      *     minimum product.
      */
     public int maxProduct(int[] nums) {
-        int[] maxSoFar = new int[nums.length];
-        int[] minSoFar = new int[nums.length];
+        int maxProduct = 1;
+        int minProduct = 1;
 
+        int result = 1;
         for(int i = 0; i < nums.length; i++) {
-            if(i == 0) {
-                maxSoFar[i] = nums[i];
-                minSoFar[i] = nums[i];
-                continue;
-            }
-
-            maxSoFar[i] = Math.max(nums[i], Math.max(maxSoFar[i - 1] * nums[i], minSoFar[i - 1] * nums[i]));
-            minSoFar[i] = Math.min(nums[i], Math.min(maxSoFar[i - 1] * nums[i], minSoFar[i - 1] * nums[i]));
-        }
-
-        int maxProduct = Integer.MIN_VALUE;
-        for(int i = 0; i < maxSoFar.length; i++) {
-            maxProduct = Math.max(maxSoFar[i], maxProduct);
+            maxProduct = Math.max(nums[i], Math.max(nums[i] * maxProduct, nums[i] * minProduct));
+            minProduct = Math.min(nums[i], nums[i] * minProduct);
+            result = Math.max(result, maxProduct);
         }
 
         return maxProduct;
--- tst/com/pablan/leetcode/medium/MaximumProductSubarrayTest.java
+++ tst/com/pablan/leetcode/medium/MaximumProductSubarrayTest.java
@@ -8,5 +8,6 @@ public class MaximumProductSubarrayTest {
     public void test() {
         MaximumProductSubarray maximumProductSubarray = new MaximumProductSubarray();
         System.out.println(maximumProductSubarray.maxProduct(new int[] {2,3,-2,4}));
+        System.out.println(maximumProductSubarray.maxProduct(new int[] {-2,0,-1}));
     }
 }
