cd534b1c897b0c3f6f145bf44a32c812a96572b4
Pablo Ochoa <pablan@amazon.com>
2020-04-08 17:35:58 -0700

    Palindromic Substrings

--- src/com/pablan/leetcode/medium/PalindromicSubstrings.java
+++ src/com/pablan/leetcode/medium/PalindromicSubstrings.java
@@ -1,5 +1,8 @@
 package com.pablan.leetcode.medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PalindromicSubstrings {
 
     /**
@@ -23,6 +26,40 @@ public class PalindromicSubstrings {
      *
      */
     public int countSubstrings(String s) {
-        return 0;
+        List<String> current = new ArrayList<>();
+        List<List<String>> totalDecomp = new ArrayList<>();
+        decomponse(0, s.length() - 1, s, current, totalDecomp);
+        return totalDecomp.size() > 0 ? totalDecomp.get(0).size() : 0;
+    }
+
+    private void decomponse(int start, int end, String s, List<String> current, List<List<String>> totalDecomp) {
+        if(start > end) {
+            totalDecomp.add(new ArrayList<>(current));
+            return;
+        }
+
+        for(int i = start; i <= end; i++) {
+            String option = s.substring(start, i + 1);
+            if(isPalindrome(option)) {
+                current.add(option);
+                decomponse(i + 1, end, s, current, totalDecomp);
+                // backtrack
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while(start <= end) {
+            if(s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
     }
 }
