34b0da2f7bbf72785fbce172644026f62722cef1
Pablo Ochoa <pablan@amazon.com>
2020-04-16 16:52:01 -0700

    Word Break explanation DP

--- src/com/pablan/leetcode/medium/WordBreak.java
+++ src/com/pablan/leetcode/medium/WordBreak.java
@@ -29,19 +29,32 @@ public class WordBreak {
      *
      */
     public boolean wordBreak(String s, List<String> wordDict) {
-        boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;
+        boolean[] breakable = new boolean[s.length() + 1];
+
+        //breakable[0] = true means empty string is always present in the dictionary
+        // in other words we represent the word leetcode as
+        // "" "l" "e" "e" "t" "c" "o" "d" "e"
+        breakable[0] = true;
 
         for(int i = 1; i <= s.length(); i++) {
+            // internal loop, this checks if from 0 to end j is breakable
+            // if it's breakable check if from start (j) to end (i) of the string is in the dictionary, if it
+            // is then breakable[i] = true
+            // For example:
+            // leetcode, i = 8 --> for j = 0 to 8
+            // when j = 4, we ask. 0 to 4(non inclusive) is breakable? "leet" is in the dict yes so we check
+            // is 4 to 8 in the dictionary? "code" is in the dictionary, therefore breakable[8] = true
             for(int j = 0; j < i; j++) {
-                if(dp[j] && wordDict.contains(s.substring(j, i))) {
-                    dp[i] = true;
-                    break;
+                if(breakable[j] == true) {
+                    if(wordDict.contains(s.substring(j, i))) {
+                        breakable[i] = true;
+                        break;
+                    }
                 }
             }
         }
 
-        return dp[s.length()];
+        return breakable[s.length()];
     }
 
     public List<List<String>> wordBreakRecursive(String s, List<String> wordDict) {
