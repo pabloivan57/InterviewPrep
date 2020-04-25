b9b4b3befd69f45b4190dc90b8d1a199f79a17c2
Pablo Ochoa <pablan@amazon.com>
2020-04-24 17:42:25 -0700

    StartOfLinkedCycle details

--- src/com/pablan/grokking/patterns/fastslow/StartLinkedListCycle.java
+++ src/com/pablan/grokking/patterns/fastslow/StartLinkedListCycle.java
@@ -1,54 +1,59 @@
 package com.pablan.grokking.patterns.fastslow;
 
+
 public class StartLinkedListCycle {
 
+    /**
+     * The trick is as follows
+     * 1.- encounter cycle length
+     * 2.- then move pointe cycle length times
+     * 4.- at this point you should be cycleLength position but we're interested
+     *     in n - cycleLength, or in other words the amount of jumps it takes from cycleLength pos to go
+     *     back in the list, which you can match by moving head one by one until they match
+     *     then return slow pointer
+     */
     public ListNode findCycleStart(ListNode head) {
+        int cycleLength = findCycleLength(head);
+
+        ListNode current = head;
+        for(int i = 0; i < cycleLength; i++) {
+            current = current.next;
+        }
+
+        ListNode slow = head;
+        ListNode fast = current;
+        while(slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+    /** The idea is as follows, once we encounter the intersection with slow and fast pointer,
+     * we can count cycle length simply by moving the pointer until we encounter intersection again
+     **/
+    public int findCycleLength(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
 
-        while(fast != null && fast.next != null) {
+        ListNode intersection = head;
+        while(fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
 
-            int cycleLength;
-            if (fast != null && slow.value == fast.value) {
-                // found cycle
-                cycleLength = calculateCycleLength(slow);
-                ListNode result = findCycleStart(head, cycleLength);
-                return result;
+            if(slow == fast) {
+                intersection = slow;
+                break;
             }
         }
 
-        return null;
-    }
-
-    private int calculateCycleLength(ListNode slow) {
-        ListNode current = slow.next;
-
-        // If next node is the same, that means we are in a self loop
-        // That by default is 1
-        int length = 1;
-        while(current != null && current.value != slow.value) {
+        int cycleLength = 1;
+        ListNode current = intersection.next;
+        while(current != intersection) {
+            cycleLength++;
             current = current.next;
-            length++;
         }
 
-        return length;
-    }
-
-    private ListNode findCycleStart(ListNode head, int cycleLength) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        for(int i = 0; i < cycleLength; i++) {
-            fast = fast.next;
-        }
-
-        while(slow.value != fast.value) {
-            slow = slow.next;
-            fast = fast.next;
-        }
-
-        return slow;
+        return cycleLength;
     }
 }
--- tst/com/pablan/grokking/patterns/fastslow/StartLinkedListCycleTest.java
+++ tst/com/pablan/grokking/patterns/fastslow/StartLinkedListCycleTest.java
@@ -15,13 +15,13 @@ public class StartLinkedListCycleTest {
         head.next.next.next.next = new ListNode(5);
         head.next.next.next.next.next = new ListNode(6);
 
-        head.next.next.next.next.next.next = head.next.next;
-        System.out.println("LinkedList cycle start: " + startLinkedListCycle.findCycleStart(head).value);
+        head.next.next.next.next.next.next = head.next.next; // 6 -> 3
+        System.out.println("LinkedList cycle start: " + startLinkedListCycle.findCycleStart(head).value); // should be 3
 
-        head.next.next.next.next.next.next = head.next.next.next;
-        System.out.println("LinkedList cycle start: " + startLinkedListCycle.findCycleStart(head).value);
+        head.next.next.next.next.next.next = head.next.next.next; // 6 -> 4
+        System.out.println("LinkedList cycle start: " + startLinkedListCycle.findCycleStart(head).value); // should be 4
 
-        head.next.next.next.next.next.next = head;
-        System.out.println("LinkedList cycle start: " + startLinkedListCycle.findCycleStart(head).value);
+        head.next.next.next.next.next.next = head; // 6 -> 1
+        System.out.println("LinkedList cycle start: " + startLinkedListCycle.findCycleStart(head).value); // should be 1
     }
 }
