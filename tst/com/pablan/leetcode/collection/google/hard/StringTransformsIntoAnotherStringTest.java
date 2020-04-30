6d9f325ffd7a930f172576e682f7471250086f15
Pablo Ochoa <pablan@amazon.com>
2020-04-29 17:35:44 -0700

    StringTransformsIntoAnotherString exercise

---
+++ tst/com/pablan/leetcode/collection/google/hard/StringTransformsIntoAnotherStringTest.java
@@ -0,0 +1,14 @@
+package com.pablan.leetcode.collection.google.hard;
+
+import org.junit.Test;
+
+public class StringTransformsIntoAnotherStringTest {
+
+    @Test
+    public void test() {
+        StringTransformsIntoAnotherString stringTransformsIntoAnotherString = new StringTransformsIntoAnotherString();
+        System.out.println(stringTransformsIntoAnotherString.canConvert("aabcc", "ccdee")); // should be true
+        // Convert 'c' to 'e' then 'b' to 'd' then 'a' to 'c'. Note that the order of conversions matter
+        System.out.println(stringTransformsIntoAnotherString.canConvert("leetcode", "codeleet")); // should be false
+    }
+}
