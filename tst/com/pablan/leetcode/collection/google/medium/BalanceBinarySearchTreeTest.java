5fd80b7af5bb7e4ed5f93b29d3866af27a9cd36e
Pablo Ochoa <pablan@amazon.com>
2020-12-10 22:45:53 -0800

    Balance Binary Search Tree

--- tst/com/pablan/leetcode/collection/google/medium/BalanceBinarySearchTreeTest.java
+++ tst/com/pablan/leetcode/collection/google/medium/BalanceBinarySearchTreeTest.java
@@ -1,4 +1,19 @@
 package com.pablan.leetcode.collection.google.medium;
 
+import com.pablan.grokking.patterns.treebfs.TreeNode;
+import org.junit.Test;
+
 public class BalanceBinarySearchTreeTest {
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.right = new TreeNode(3);
+        root.right.right.right = new TreeNode(4);
+
+        BalanceBinarySearchTree balance = new BalanceBinarySearchTree();
+        TreeNode balancedRoot = balance.balanceBST(root);
+        System.out.println(balancedRoot);
+    }
 }
