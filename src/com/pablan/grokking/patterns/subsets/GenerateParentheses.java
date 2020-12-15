d00e71efa1e42b2629d279670a4f99b8d4124ca5
Pablo Ochoa <pablan@amazon.com>
2020-12-14 22:08:50 -0800

    Generate Parentheses Grokking

--- src/com/pablan/grokking/patterns/subsets/GenerateParentheses.java
+++ src/com/pablan/grokking/patterns/subsets/GenerateParentheses.java
@@ -7,23 +7,72 @@ import java.util.Queue;
 
 public class GenerateParentheses {
 
+    /**
+     *  For a given number ‘N’, write a function to generate all combination
+     *  of ‘N’ pairs of balanced parentheses.
+     *
+     *  Example 1:
+     *
+     *  Input: N=2
+     *  Output: (()), ()()
+     *  Example 2:
+     *
+     *  Input: N=3
+     *  Output: ((())), (()()), (())(), ()(()), ()()()
+     *
+     *  Pablo's notes: This is a bfs exploratory sort of like playing a game.
+     *  We keep each state and we attempt to generate next state based on rules.
+     *
+     *  In this example, we start from the fact that:
+     *  1.- You cannot close a parentheses that you haven't opened... therefore we always check if we can
+     *      add a left
+     *
+     *  2.- As long as there are more lefts than right on the expression, we can close them
+     *
+     *  So we can go as follows
+     *
+     *     (
+     *  |     \
+     *  ((    ()
+     *  |      |
+     *  (()    ()(
+     *  |       |
+     *  (())    ()()
+     */
     public List<String> generateValidParentheses(int num) {
+        Queue<Parentheses> queue = new LinkedList<>();
+        queue.offer(new Parentheses("", 0, 0));
+
         List<String> result = new ArrayList<>();
-        Queue<ParenthesesString> queue = new LinkedList<>();
-        queue.add(new ParenthesesString("", 0, 0));
+
         while(!queue.isEmpty()) {
-            ParenthesesString ps = queue.poll();
-            // if we've reached the maximum number of open and close parentheses, add to the result
-            if (ps.openCount == num && ps.closeCount == num) {
-                result.add(ps.str);
-            } else {
-              if (ps.openCount < num) // if we can add an open parentheses, add it
-                queue.add(new ParenthesesString(ps.str + "(", ps.openCount + 1, ps.closeCount));
-              if (ps.openCount > ps.closeCount) // if we can add a close parentheses, add it
-                queue.add(new ParenthesesString(ps.str + ")", ps.openCount, ps.closeCount + 1));
+            Parentheses current = queue.poll();
+            if(current.rightCount == num && current.leftCount == num) {
+                result.add(current.str);
+                continue;
+            }
+
+            if(current.leftCount < num) {
+                queue.offer(new Parentheses(current.str + "(", current.leftCount + 1, current.rightCount));
+            }
+
+            if(current.leftCount > current.rightCount) {
+                queue.offer(new Parentheses(current.str + ")", current.leftCount, current.rightCount + 1));
             }
         }
 
         return result;
+    }
+
+    private class Parentheses {
+        String str;
+        int leftCount;
+        int rightCount;
+
+        Parentheses(String str, int leftCount, int rightCount) {
+            this.str = str;
+            this.leftCount = leftCount;
+            this.rightCount = rightCount;
+        }
     }
 }
