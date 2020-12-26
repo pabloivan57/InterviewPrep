9600dbef91d6d1eac1b54fc553083328c7fd6b33
Pablo Ochoa <pablan@amazon.com>
2020-12-26 13:24:50 -0800

    Order Agnostic Binary Search comments

--- tst/com/pablan/grokking/patterns/modifiedbinary/OrderAgnosticBinarySearchTest.java
+++ tst/com/pablan/grokking/patterns/modifiedbinary/OrderAgnosticBinarySearchTest.java
@@ -6,9 +6,9 @@ public class OrderAgnosticBinarySearchTest {
 
     @Test
     public void test() {
-        //System.out.println(new OrderAgnosticBinarySearch().search(new int[] { 4, 6, 10 }, 10));
-        //System.out.println(new OrderAgnosticBinarySearch().search(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 5));
-        //System.out.println(new OrderAgnosticBinarySearch().search(new int[] { 10, 6, 4 }, 10));
+        System.out.println(new OrderAgnosticBinarySearch().search(new int[] { 4, 6, 10 }, 10));
+        System.out.println(new OrderAgnosticBinarySearch().search(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 5));
+        System.out.println(new OrderAgnosticBinarySearch().search(new int[] { 10, 6, 4 }, 10));
         System.out.println(new OrderAgnosticBinarySearch().search(new int[] { 10, 6, 4 }, 4));
     }
 }
