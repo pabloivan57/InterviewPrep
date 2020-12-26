c83ba660b3fe4be3f2bdc6b70a20cfca12855f54
Pablo Ochoa <pablan@amazon.com>
2020-12-26 14:06:56 -0800

    Ceiling of a number problem comments

--- src/com/pablan/grokking/patterns/modifiedbinary/CeilingOfANumber.java
+++ src/com/pablan/grokking/patterns/modifiedbinary/CeilingOfANumber.java
@@ -2,24 +2,65 @@ package com.pablan.grokking.patterns.modifiedbinary;
 
 public class CeilingOfANumber {
 
+    /**
+     *  Given an array of numbers sorted in an ascending order, find the ceiling of a given number ‘key’.
+     *  The ceiling of the ‘key’ will be the smallest element in the given array greater than or equal to the ‘key’.
+     *
+     *  Write a function to return the index of the ceiling of the ‘key’. If there isn’t any ceiling return -1.
+     *
+     *  Example 1:
+     *
+     *  Input: [4, 6, 10], key = 6
+     *  Output: 1
+     *  Explanation: The smallest number greater than or equal to '6' is '6' having index '1'.
+     *  Example 2:
+     *
+     *  Input: [1, 3, 8, 10, 15], key = 12
+     *  Output: 4
+     *  Explanation: The smallest number greater than or equal to '12' is '15' having index '4'.
+     *  Example 3:
+     *
+     *  Input: [4, 6, 10], key = 17
+     *  Output: -1
+     *  Explanation: There is no number greater than or equal to '17' in the given array.
+     *  Example 4:
+     *
+     *  Input: [4, 6, 10], key = -1
+     *  Output: 0
+     *  Explanation: The smallest number greater than or equal to '-1' is '4' having index '0'.
+     */
     public int searchCeilingOfANumber(int[] arr, int key) {
-        if (key > arr[arr.length - 1]) // if the 'key' is bigger than the biggest element
-            return -1;
+        int low = 0;
+        int high = arr.length - 1;
 
-        int start = 0, end = arr.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if(key < arr[mid]) {
-                end = mid - 1;
-            } else if (key > arr[mid]) {
-                start = mid + 1;
+        // edge case, if the key is too high and not in the array
+        // the algorithm will reach the far right and check if number is more than key
+        // because it's now it will go beyond bound and while loop will exit. However,
+        // the low at this point will be arr.length + 1 which is incorrect because problem
+        // wants -1
+        if(key > arr[high]) {
+            return -1;
+        }
+
+        while(low <= high) {
+            int middle = low + ((high - low) / 2);
+
+            // ceiling of a number is that same number
+            if(arr[middle] == key) {
+                return middle;
+            }
+
+            if(arr[middle] > key) {
+                // search left
+                high = middle - 1;
             } else {
-                return mid;
+                // search right
+                low = middle + 1;
             }
         }
 
-        // since the loop is running until 'start <= end', so at the end of the while loop, 'start == end+1'
-        // we are not able to find the element in the given array, so the next big number will be arr[start]
-        return start;
+        // binary search will stop when low == high. that will be the ceiling if
+        // number is not found on array. return low or high
+        return low;
     }
 }
