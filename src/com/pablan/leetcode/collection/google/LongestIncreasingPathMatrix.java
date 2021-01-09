a69814fb492b5f8cdc60d47f51144a4837927e86
Pablo Ochoa <pablan@amazon.com>
2021-01-09 12:26:28 -0800

    LongestIncreasingPathMatrix

--- src/com/pablan/leetcode/collection/google/LongestIncreasingPathMatrix.java
+++ src/com/pablan/leetcode/collection/google/LongestIncreasingPathMatrix.java
@@ -1,10 +1,10 @@
 package com.pablan.leetcode.collection.google;
 
 import java.util.Arrays;
+import java.util.Map;
 
 public class LongestIncreasingPathMatrix {
 
-    int maxPath = Integer.MIN_VALUE;
     /**
      * Given an integer matrix, find the length of the longest increasing path.
      *
@@ -33,39 +33,50 @@ public class LongestIncreasingPathMatrix {
      * Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
      *
      */
+    // up, down, left, right
+    int[][] directions = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
     public int longestIncreasingPath(int[][] matrix) {
         int[][] dp = new int[matrix.length][matrix[0].length];
         for(int i = 0; i < dp.length; i++) {
-            Arrays.fill(dp[i], -1);
+            Arrays.fill(dp[i], - 1);
         }
 
+        int maxPath = -1;
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix[0].length; j++) {
                 boolean[][] visited = new boolean[matrix.length][matrix[0].length];
-                bfs(i, j, Integer.MIN_VALUE, 0, visited, matrix);
+                maxPath = Math.max(maxPath, dfs(i, j, matrix, visited, -1, 0, dp));
             }
         }
-
+        
         return maxPath;
     }
 
-    public void bfs(int row, int column, int parent, int pathSize, boolean[][] visited, int[][] matrix) {
+    private int dfs(int row, int column,int[][] matrix, boolean visited[][], int parent, int pathCount, int[][] dp) {
         if(row < 0
                 || row >= matrix.length
                 || column < 0
                 || column >= matrix[0].length
                 || visited[row][column]
-            || parent >= matrix[row][column]) {
-            maxPath = Math.max(maxPath, pathSize);
-            return;
+                || parent > matrix[row][column]) {
+            return pathCount;
         }
 
-        // visit
+        if(dp[row][column] != -1) {
+             return dp[row][column] + pathCount;
+        }
+
         visited[row][column] = true;
 
-        bfs(row + 1, column, matrix[row][column], pathSize + 1, visited, matrix);
-        bfs(row - 1, column, matrix[row][column], pathSize + 1, visited, matrix);
-        bfs(row, column + 1, matrix[row][column], pathSize + 1, visited, matrix);
-        bfs(row, column - 1, matrix[row][column], pathSize + 1, visited, matrix);
+        for(int i = 0; i < directions.length; i++) {
+            int[] direction = directions[i];
+            dp[row][column] = Math.max(dp[row][column],
+                    dfs(row + direction[0], column + direction[1], matrix, visited, matrix[row][column], pathCount + 1, dp));
+        }
+
+        // backtrack
+        visited[row][column] = false;
+        return dp[row][column];
     }
 }
