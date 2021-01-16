b7801910214d4881a43b39fe286a8c8e0dedebe3
Pablo Ochoa <pablan@amazon.com>
2021-01-15 16:40:41 -0800

    Expressive Words

--- src/com/pablan/leetcode/collection/google/medium/ExpressiveWords.java
+++ src/com/pablan/leetcode/collection/google/medium/ExpressiveWords.java
@@ -30,6 +30,34 @@ public class ExpressiveWords {
      *
      * Pablo's notes: First, the problem is not that clear
      * They are asking to identify which on from those words can be stretched out to match S
+     *
+     * Ok, so this is straightforward, but there are a couple use cases to consider
+     *
+     * Let's compare heeellooo : hello
+     * h with h. matches. Now, it could be that work is streetchy... so let's count
+     * how many hs are on heeellooo. 1. Cool
+     * Now let's count how many hs are in hello. 1. Cool
+     *
+     * The constraint says that a word can be made stretchy is it's repeated more than 3 characters
+     * That is because you don't want an l to be counted as stretchy when ll is part of a normal word
+     * In this case we can 1 : 1, are they equal? yes... cool let's keep going
+     * 1.- As a corollary of the above, if left are <= 2 size... they right must be equal to match
+     *
+     * Now, let's analyze e with e on heeellooo : he
+     *                                 ^           ^
+     * They match, not let's see if its stretchy
+     * left side... turns out to be 'eee'
+     * right side... is 'e'
+     * left side is more than right side 3 > 1. That means, we could stretch right side to be eee as well. So cool
+     * let's jump eee on the left side and keep moving on to analyze heeellooo : hello
+     *                                                                   ^         ^
+     *  Before doing that, what would happen if in the right side we had 4 e's? For example:
+     *  heeellooo : heeeellooo
+     *  We would be comparing 'eee' > 'eeee'. In this case we cannot match because we are missing an e on left side
+     *  So that gives us a rule. That we already applied above. If leftSide > 3 && rigthSide < leftSide, go on
+     *
+     *  And we keep doing this until the end of the word
+     *
      */
     public int expressiveWords(String S, String[] words) {
         int stretchyWords = 0;
