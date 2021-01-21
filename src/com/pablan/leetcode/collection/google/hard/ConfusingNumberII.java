aa3e6b4b925dc3813d1610b0975949941dc9134a
Pablo Ochoa <pablan@amazon.com>
2021-01-20 18:39:25 -0800

    Confusing Number explanation

--- src/com/pablan/leetcode/collection/google/hard/ConfusingNumberII.java
+++ src/com/pablan/leetcode/collection/google/hard/ConfusingNumberII.java
@@ -1,8 +1,10 @@
 package com.pablan.leetcode.collection.google.hard;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 
 public class ConfusingNumberII {
@@ -36,8 +38,82 @@ public class ConfusingNumberII {
      * Output: 19
      * Explanation:
      * The confusing numbers are [6,9,10,16,18,19,60,61,66,68,80,81,86,89,90,91,98,99,100].
+     *
+     * Pablo's notes: The intuition of this problem is as follows. How do you construct Confusing numbers?
+     *
+     * 1.- Add 0, 1, 6, 8 or 9 to each previously achieve number. Let's start from 0.
+     *
+     * 0 + 0 / 0 == 0 ? Not confusing
+     * 0 + 1 / 1 == 1 ? Not confusing
+     * 0 + 6 / 6 == 9 ? Confusing
+     * 0 + 8 / 8 == 8 ? Not confusing
+     * 0 + 9 / 9 == 6 Confusing
+     *
+     * Difference is, we might have a confusing if we keep going and check what would happen with the resulting numbers.
+     * Example:
+     * 1 + 0 / 10 != 01 ? Confusing
+     * 1 + 1 / 11 == 11 Not confusing
+     * 1 + 6 / 16 != 61 Confusing
+     * .
+     * .
+     * We stop at N
+     * 11 + 1 / 111 > 20. We stop
+     *
+     * So we start with 0. at each level we try 0, 1, 6, 8, 9 and we keep going recursively until we hit a N
+     * and at each stage we check for confusing number
+     *
+     * 2. - How to we check for confusing number?
+     * 68 == 89
+     * 89 == 8 * 10 + 9 --> How did we get to 9? well we now that 6 maps to 9 and we worked backwards from 68
+     *                                                                                                      ^
+     *  basically num % 10, take that num, flip it and multiply by 10
      */
+    Map<Integer, Integer> mappings;
+    int result = 0;
+
+    public ConfusingNumberII() {
+        mappings = new HashMap<>();
+        mappings.put(0, 0);
+        mappings.put(1, 1);
+        mappings.put(6, 9);
+        mappings.put(8, 8);
+        mappings.put(9, 6);
+    }
+
     public int confusingNumberII(int N) {
-        return 0;
+        result = 0;
+
+        helper(0, N);
+
+        return result;
+    }
+
+    private void helper(int current, int N) {
+        if(isConfusing(current)) {
+            result++;
+        }
+
+        for(Integer key : mappings.keySet()) {
+            int nextConfusing = current * 10 + key;
+            // we already explored 0 in this level. If we do 0 * 0 + anything we will explore the same
+            // that's why we skip 0, to save a couple cycles
+            if(nextConfusing < N && nextConfusing != 0) {
+                helper(nextConfusing, N);
+            }
+        }
+    }
+
+    private boolean isConfusing(int number) {
+        // number is confusing if flipped is different than the original
+        // example: 69 != 96, 10 != 01
+        int flipped = 0;
+        while(number > 0) {
+            int lastNumber = number % 10;
+            int mirroredNumber = mappings.get(lastNumber);
+            flipped = flipped * 10 + mirroredNumber;
+            number = number / 10;
+        }
+
+        return flipped != number;
     }
 }
