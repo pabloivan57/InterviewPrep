bc27f8db8473c645f0c9fd1ae27b121c7bab175a
Pablo Ochoa <pablan@amazon.com>
2021-02-13 13:19:35 -0800

    Dutch National Flag Test correction

--- tst/com/pablan/leetcode/medium/DutchNationalFlagTest.java
+++ tst/com/pablan/leetcode/medium/DutchNationalFlagTest.java
@@ -12,5 +12,9 @@ public class DutchNationalFlagTest {
         DutchNationalFlag dutchNationalFlag = new DutchNationalFlag();
         dutchNationalFlag.sort(array);
         System.out.println(Arrays.toString(array));
+
+        int[] array2 = new int[] {2, 2, 0, 1, 2, 0};
+        dutchNationalFlag.sort(array2);
+        System.out.println(Arrays.toString(array2));
     }
 }
