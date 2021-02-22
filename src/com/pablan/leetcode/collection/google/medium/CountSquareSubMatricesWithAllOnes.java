bdd46d6e99f4ae1969cc4d7907bca796ef24afe3
Pablo Ochoa <pablan@amazon.com>
2021-02-21 21:58:59 -0800

    CountSquareSubMatricesWithAllOnes

--- src/com/pablan/leetcode/collection/google/medium/CountSquareSubMatricesWithAllOnes.java
+++ src/com/pablan/leetcode/collection/google/medium/CountSquareSubMatricesWithAllOnes.java
@@ -13,6 +13,8 @@ public class CountSquareSubMatricesWithAllOnes {
      *   [1,1,1,1],
      *   [0,1,1,1]
      * ]
+     *
+     *
      * Output: 15
      * Explanation:
      * There are 10 squares of side 1.
@@ -32,8 +34,72 @@ public class CountSquareSubMatricesWithAllOnes {
      * There is 1 square of side 2.
      * Total number of squares = 6 + 1 = 7.
      *
+     *  Pablo's DP explanation:
+     *  The best explanation for DP is https://leetcode.com/problems/count-square-submatrices-with-all-ones/discuss/643429/Python-DP-Solution-%2B-Thinking-Process-Diagrams-(O(mn)-runtime-O(1)-space)
+     *
+     *  Basically, imagine you can make "combos" only if the 3 surrounding values around a cell are "1"
+     *
+     *  1 ==> 1 square
+     *
+     *  1 1
+     *  1 (1)     ==> Here we have a combo because all sorrounding of 1,1 are 1.. therefore we actually have 2 squares here
+     *    |
+     *    v
+     *   transforms to
+     *
+     *  1  1
+     *  1  2
+     *
+     *  The result then is just the sum of all values computed. In other words 5
+     *
+     *  Now look at a better example:
+     *
+     *  ______
+     *| 1 0   1
+     *| 1 (1) 0    We don't even bother with edges because we know they cannot start a combo. So we start at pos 1,1
+     *| 1 1   0
+     *   |
+     *   v
+     *  here we cannot make a combo. So turns out the cell itself is as many squares as I can make
+     *
+     *  1 0  1
+     *  1 1  0    ... we keep moving, the next valid one is 2,1
+     *  1 1  0
+     *
+     *   |
+     *   V
+     *
+     *  1 0   1
+     *  1 1   0  .. here we can make a combo. Turns out 2,1 equals 2 squares
+     *  1 (1) 0
+     *
+     *   |
+     *   v
+     *
+     *  1 0 1
+     *  1 1 0   .. Total squares equals sum of all values = 7
+     *  1 2 0
      */
+
     public int countSquares(int[][] matrix) {
+        int result = 0;
+
+        for(int row = 0; row < matrix.length; row++) {
+            for(int col = 0; col < matrix[0].length; col++) {
+                if(matrix[row][col] == 1) {
+                    if(row > 0 && col > 0) {
+                        // this could make a combo
+                        matrix[row][col] = Math.min(matrix[row - 1][col - 1], Math.min(matrix[row - 1][col], matrix[row][col - 1])) + 1;
+                    }
+                    result += matrix[row][col];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public int countSquaresLame(int[][] matrix) {
         int totalSquares = 0;
 
         for(int row = 0; row < matrix.length; row++) {
